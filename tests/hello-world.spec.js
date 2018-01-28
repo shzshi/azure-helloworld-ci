5b6b7d799a4243a687af1cf18a50d2d93eb1edbc
Shashikant Bangera <shashikant.bangera@capgemini.com>
2018-01-28 02:07:55 +0000

    Cleared some test

--- tests/hello-world.spec.js
+++ tests/hello-world.spec.js
@@ -1,22 +1,3 @@
-const helloWorld = require('../handler').helloWorld;
-
-
-describe('helloWorld', () => {
-    var event = {};
-    var context = {};
-    var resp = {};
-    
-    it('should call helloWorld function with success', (done) => {
-        var callback = (ctx, data) => {
-            console.log(data);
-            resp = data;
-            done();
-        }
-        helloWorld(event, context, callback);
-        expect(resp.statusCode).toBe(200);
-    });
-});
-
 const HelloWorld = require('../lib/hello-world');
 
 describe('sayHello', () => {
